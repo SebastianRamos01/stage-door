014f05fc9fbf701563311ffce9b7ef4f00f4c6af
SebastianRamos01 <sebasr.sr@hotmail.com>
2023-12-07 23:19:55 -0300

    contact page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,51 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div className='main'>Soy About</div>
+    <div className='main'>
+      <div>
+        Our Team  
+      </div>
+      <ul>
+        <li className="members">
+          <div>
+            Web Developers
+          </div>
+          <div>
+            Lorem Ipsum
+          </div>
+          <div>
+            Lorem Ipsum
+          </div>
+        </li>
+        <li className="members">
+          <div>
+            Web Designers
+          </div>
+          <div>
+            Lorem Ipsum
+          </div>
+          <div>
+            Lorem Ipsum
+          </div>
+          <div>
+            Lorem Ipsum
+          </div>
+        </li>
+        <li className="members">
+          <div>
+            Web Designers
+          </div>
+          <div>
+            Lorem Ipsum
+          </div>
+          <div>
+            Lorem Ipsum
+          </div>
+          <div>
+            Lorem Ipsum
+          </div>
+        </li>  
+      </ul>  
+    </div>
   )
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,6 +2,36 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>Soy Contact</div>
+    <div className='main'>
+      <div className='contact-text'>
+        We are happy to talk!
+      </div>
+      <ul className=''>
+        <li className='contact'>
+          <div>
+            Phone
+          </div>
+          <div className='info'>
+            +11 35838175
+          </div>
+        </li>
+        <li className='contact'>
+          <div>
+            Email
+          </div>
+          <div className='info'>
+            sebasm.sr@gmail.com
+          </div>
+        </li>
+        <li className='contact'>
+          <div className=''>
+            Follow
+          </div>
+          <div className='info'>
+            _sbasramos
+          </div>
+        </li>
+      </ul>
+    </div>
   )
 }
